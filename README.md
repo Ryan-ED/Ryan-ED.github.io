1f3fc8a3a32d8e8736c5ec378796d509de80e0c2
Ryan <ryan.aka.spike@gmail.com>
2017-07-31 12:54:44 +0200

    added files from codepen export
    so many unecessary commits :/

---
+++ README.md
@@ -0,0 +1,3 @@
+# A personal portfolio webpage for Mr. Ryan Daniels.
+
+## A Pen created at [CodePen.io](http://codepen.io). You can find this one [here](http://codepen.io/RyanED/pen/PjBdWd).
---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("nav a").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $($.attr(this,"href")).offset().top - 60
+      }, 1500);
+  });
+});
